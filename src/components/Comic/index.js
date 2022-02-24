084c15cb4e15db35bd19b3da20df605c0361de07
Alejandra Vargas <alejandramariavari@gmail.com>
2022-02-23 22:09:06 -0500

    fix RatingStars component

--- src/components/Comic/index.js
+++ src/components/Comic/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import './Comic.scss'
 
 // component
 import RatingStars from '../RatingStars'
 
 export default function Comic() {
+
+    const idStarsArray = [1,2,3,4,5];
+    const [rating, setRating] = useState(0);
+    const [hoverRating, setHoverRating] = useState(0);
+
+
+    const eventMouseEnter = (starId) => {
+        setHoverRating(starId);
+    };
+
+    const eventMouseLeave = () => {
+        setHoverRating(0);
+    }
+
+    const onClickStar = (starId) => {
+        setRating(starId);
+    }
+
+    console.log("rating", rating);
+
+    
     return ( 
         <section>
             <h1>Title Comic</h1>
@@ -12,7 +33,25 @@ export default function Comic() {
                 <img src="https://via.placeholder.com/350" alt="imageComic"/>
             </div>
             
-            <RatingStars />
+            <div className="containerStars">
+
+                {idStarsArray.map((starId) => {
+
+                    return(
+                        <RatingStars
+                            key={starId}
+                            starId={starId}
+                            rating = {rating}
+                            hoverRating = {hoverRating}
+                            eventMouseEnter = {eventMouseEnter}
+                            eventMouseLeave = {eventMouseLeave}
+                            onClickStar={onClickStar}
+                        />
+                    )
+                })}
+
+            </div>
+
         </section>
     )
 }
